000d5bb13d6db78706f71b82c712e0acc344371e
unknown <davidjmartin@hotmail.com>
2020-11-21 21:18:42 +0000

    first commit

---
+++ src/main/java/com/apidrivenfirst/demo/ApiDrivenFirstApplication.java
@@ -0,0 +1,13 @@
+package com.apidrivenfirst.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDrivenFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDrivenFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apidrivenfirst/demo/ApiDrivenFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidrivenfirst.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDrivenFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
