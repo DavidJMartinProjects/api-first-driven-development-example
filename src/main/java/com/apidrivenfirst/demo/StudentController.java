21e294932182d5724179827f1a810db6332db1a1
unknown <davidjmartin@hotmail.com>
2020-11-21 22:48:24 +0000

    hooking up generated interface to controller

---
+++ src/main/java/com/apidrivenfirst/demo/StudentController.java
@@ -0,0 +1,16 @@
+package com.apidrivenfirst.demo;
+
+import org.openapitools.api.StudentsApi;
+import org.openapitools.model.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController implements StudentsApi {
+
+	@Override
+	public ResponseEntity<Student> getStudents() {
+		return new ResponseEntity<Student>(new Student().id(100).name("John Doe").age(30), HttpStatus.OK);
+	}
+}
